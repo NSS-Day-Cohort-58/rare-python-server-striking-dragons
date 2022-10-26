660328398bf573b16bbeee8c251d56a114c891f4
James Scott <jsa.codes@gmail.com>
2022-10-26 15:36:38 -0500

    Created create_tag function in tag_requests.py

--- views/tag_requests.py
+++ views/tag_requests.py
@@ -54,3 +54,27 @@ def get_single_tag(id):
         tag = Tag(data['id'], data['label'])
 
         return tag.__dict__
+
+def create_tag(new_tag):
+  with sqlite3.connect("./db.sqlite3") as conn:
+
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute(
+          """
+        INSERT INTO TAGS
+          (label)
+        VALUES
+          ( ? );
+          """,
+          (
+            new_tag["label"]
+          ),
+        )
+
+        id = db_cursor.lastrowid
+
+        new_tag["id"] = id
+
+  return new_tag
