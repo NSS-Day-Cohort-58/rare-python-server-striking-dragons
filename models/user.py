317e6314a94296cd9f9d572d15c8cd100c24b2d3
Amanda De Leon <mandapanda6991@yahoo.com>
2022-10-26 10:07:24 -0500

    added the user class in user.py

--- models/user.py
+++ models/user.py
@@ -1,11 +1,11 @@
 class User():
     def __init__(self, id, username, first_name, last_name, email, password, profile_image_url, bio, created_on):
-        self.id = id,
-        self.username = username,
-        self.first_name = first_name,
-        self.last_name = last_name,
-        self.email = email,
-        self.password = password,
-        self.profile_image_url = profile_image_url,
-        self.bio = bio,
+        self.id = id
+        self.username = username
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.profile_image_url = profile_image_url
+        self.bio = bio
         self.created_on = created_on
